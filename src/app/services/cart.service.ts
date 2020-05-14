52d312fea84ea85fd3cf3ccc36acabe69d6c51bf
insalespacecat <60538056+insalespacecat@users.noreply.github.com>
2020-05-15 01:29:58 +0300

    minor changes

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -6,17 +6,9 @@ import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-
-//Logic that has to provide functionality
-//for "beautiful sticky cart that can be shown and hidden by pressing a button" is commented out because
-//1)it is not working properly 2) i was unable to implement this type of layout at the moment because i am noob and dkwtd:
-//"sticky" property does not work properly (angular issue), position: fixed makes the layout not scalable
-//button does not fit mat-grid tile properly
-//if i'm trying to remove the cart from mat-gird-list, and place it somewhere else it becomes not clickable
-//(in navbar component) for example.
-
+//moved into purchase process
 export class CartService {
-
+/*
   private cart: Array<CartItemInterface> = [];
   cartTotalPrice = 0;
   itemIndex: number;
@@ -31,7 +23,7 @@ export class CartService {
     this.cart.forEach(item => { this.cartTotalPrice += item.price; });
     return this.cartTotalPrice;
   }
-  /*
+
   firstProductAdded() {
     if (this.firstProductExists) {
       return false;
@@ -40,12 +32,11 @@ export class CartService {
       return true;
     }
   }
-   */
-  /*
+
   hasProducts() {
     return this.cart.length > 0;
   }
-   */
+
   add(cartItem: CartItemInterface) {
     this.cart.forEach(elem => {
       if (elem.id === cartItem.id) {
@@ -68,11 +59,9 @@ export class CartService {
   remove(index: number) {
     this.cart.splice(index, 1);
     this.patchSync();
-    /*
     if (this.cart.length === 0) {
       this.firstProductExists = false;
     }
-     */
     return this.get();
   }
   postSync() {
@@ -91,4 +80,5 @@ export class CartService {
     return this.http.delete<Array<CartItemInterface>>(this.cartAPIURL + '/syncDelete', {withCredentials: true})
       .subscribe(res => this.cart = res);
   }
+  */
 }
